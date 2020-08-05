cad93c409d5cbce8fdccdf3ea4f7f2c73c508b36
minerva-hieu <hieunq@minerva.vn>
2020-08-05 09:39:43 +0700

    fix merge

--- src/pages/Dashboard/Card/CardProjectList.js
+++ src/pages/Dashboard/Card/CardProjectList.js
@@ -9,26 +9,37 @@ import { useDispatch, useSelector } from "react-redux";
 const CardProjectList = (props) => {
     
     return (
-        <div className={props.className ? props.className : ''}>
-            <CardHeader label="ProjectFilterTitle" />
-            {
-                regionSuccess &&
+        <div className="row">
+            <div className="col-12">
                 <div className="card square">
                     <div className="card-body">
-                        <div className="row">
-                            <InputSelect className="col-xl-3 col-lg-6 col-md-12 col-sm-12" value={region === null ? '' : region} name="region" label="ByRegionTitle" placeholder="RegionHintText" options={regionData} onChange={onChange} />
-                            <InputSelect className="col-xl-3 col-lg-6 col-md-12 col-sm-12" value={province === null ? '' : province} name="province" label="ByProvinceTitle" placeholder="ProvinceHintText" options={provinceData} onChange={onChange} />
-                            <InputSelect className="col-xl-3 col-lg-6 col-md-12 col-sm-12" value={type === null ? '' : type} name="type" label="ByProjectTypeTitle" placeholder="ProjectTypeHintText" options={typeData} onChange={onChange} />
-                            <InputSelect className="col-xl-3 col-lg-6 col-md-12 col-sm-12" value={status === null ? '' : status} name="status" label="ByProjectStatusTitle" placeholder="ProjectStatusHintText" options={statusData} onChange={onChange} />
-                        </div>
-                        <div className="row">
-                            <div className="col-12">
-                                <button type="submit" style={{float: "right"}} className="btn-uni-purple"><Trans>Search</Trans></button>
+                        <div className="m_table m_table--list_project">
+                            <div className="table-responsive">
+                                <table className="table table-detail-project m_table" id="list_projects">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col"><Trans>No.</Trans></th>
+                                            <th scope="col"><Trans>Project Name</Trans></th>
+                                            <th scope="col"><Trans>Project Status</Trans></th>
+                                            <th scope="col"><Trans>Updated At</Trans></th>
+                                            <th scope="col"><Trans>Updated By</Trans></th>
+                                            <th scope="col"><Trans>Detail</Trans></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        this.state.datas && this.state.datas.map((item, index) => (
+                                            <RowProject key={index} data={item} serial={index + 1} />
+                                        ))
+                                    }
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
+                        <Pagination total_page={this.state.total_page} total_record={this.state.total_record} page={this.state.page} change={this.changePage} />
                     </div>
                 </div>
-            }
+            </div>
         </div>
     )
 }
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -10,20 +10,12 @@ class Dashboard extends Component {
         return (
             <div>
                 <SalesSummary />
-                {/* <div className="row">
-                    <CardMap user={this.props.user} showToast={this.props.showToast} search={this.props.search} updateLocation={this.props.updateLocation} />
-                </div> */}
-
                 <div className="row">
                     <div className="col-xl-9 col-lg-9 col-md-12 col-sm-12 d-flex flex-column">
                         <CardHeader label="ProjectFilterTitle" />
                         <CardFilter token="MjoxMzliMDZiZmI4OTJhOGYxYmQ2MzVhZmFmODEyZmM5M2RhNDFkM2Yx" />
                     </div>
                 </div>
-
-                {/* <div className="row">
-                    <CardTimeline user={this.props.user} showToast={this.props.showToast} search={this.props.search} updateLocation={this.props.updateLocation}/>
-                </div> */}
                 <div className="row mt-2">
                     <ChartSell />
                     <ChartRevenua />
