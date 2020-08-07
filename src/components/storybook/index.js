8541def62ae60695fa7e9a9e7ad8e1917a0d665f
nhutnhm@minerva.vn <nhutnhm@minerva.vn>
2020-08-07 18:48:39 +0700

    add storybook

---
+++ src/components/storybook/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import Select from '../base/Select/Select';
+
+const storyBook = props => {
+
+    return (
+        <div>
+            <Select />
+        </div>
+    )
+}
+
+export default storyBook;
