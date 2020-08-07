cfd288627d2ce4fc6345f15d2067b5d3c0471d77
HuyNguyen1701 <huynt@minerva.vn>
2020-08-07 10:11:09 +0700

    no message

--- src/components/Card/ListProduct.js
+++ src/components/Card/ListProduct.js
@@ -1,9 +1,13 @@
-import React from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from 'react';
 import Pagination from "../common/Pagination"
 import { Trans } from 'react-i18next';
-import ListProductDetail from "./ListProductDetail"
+import ListProductDetail from "./ListProductDetail";
+import ListProductTable from "./ListProductTable";
 const body = props => {
     let { dataFilter, data, page, total_page, total_record } = props;
+    const [state, setState] = useState(true);
+
     return (
         <div className="m_table--wrapper">
             <div className="m_filter pb-4">
@@ -21,20 +25,23 @@ const body = props => {
                     </ul>
                     <ul className="m_filter--show mt-xl-0 mt-2">
                         <li className="first"><span><Trans>Displayed by</Trans></span></li>
-                        <li className=" active "><a><i className="la la-th"></i></a></li>
-                        <li className=""><a><i className="la la-th-list"></i></a></li>
+                        <li className={state ? "active" : ""} onClick={() => setState(true)}><a><i className="la la-th"></i></a></li>
+                        <li className={state ? "" : "active"} onClick={() => setState(false)}><a><i className="la la-th-list"></i></a></li>
                     </ul>
                 </div>
             </div>
 
-
-            <div className="m_grid m_grid--list_project">
+            {state ? <div className="m_grid m_grid--list_project">
                 <div className="row">
                     {data.length > 0 ? data.map((item, index) => {
                         return <ListProductDetail item={item} index={index} key={index} />
                     }) : null}
                 </div >
-            </div >
+            </div > :
+                <div className="m_table m_table--sales" style={{ maxHeight: '100%' }}>
+                    <ListProductTable data={data} />
+                </div>}
+
             <Pagination
                 total_page={total_page}
                 total_record={total_record}
---
+++ src/components/Card/ListProductTable.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Trans } from 'react-i18next';
+import { Link } from "react-router-dom"
+
+const ListProductTable = props => {
+    const trPerant = (data, index) => {
+        return (
+            <tr key={data.about + index}>
+                <td className="text-left align-middle">{index}</td>
+                <td className="text-left fw-medium align-middle">
+                    <Link to={`/cart/cart_list/detail/${data.project_id}`} className="link_href_6d30ab" >{data.about}</Link>
+                </td>
+                <td className="text-left align-middle" style={{ color: '#a8c200' }}>{data.setting_type_name}</td>
+                <td className="text-left align-middle">{data.investor_name}</td>
+                <td className="text-left align-middle">{data.address}</td>
+                <td className="text-left align-middle">{data.project_duration_type ? data.project_duration_type : "-"}</td>
+                <td className="text-left align-middle">{data.project_acreage}<sup>2</sup> </td>
+                <td className="text-left align-middle">{data.number_of_floor} (T)</td>
+                <td className="text-left align-middle">{data.building_density}%</td>
+                <td className="text-center align-middle">{data.total_area}</td>
+                <td className="text-center align-middle">
+                    <a className="uni_text_6d30ab" href="/backend/management/category/project/13/" title="Edit project">
+                        {/* <i className="edit fs-20 icon las la-pen" /> */}
+                    </a>
+                    <Link to={`/cart/cart_list/detail/${data.project_id}`} className="uni_text_6d30ab" title="News project">
+                        <i className="news fs-20 icon las la-newspaper" />
+                    </Link>
+                </td>
+            </tr>
+        )
+    }
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="text-uppercase"><Trans>No.</Trans></th>
+                    <th className="text-uppercase">Project name</th>
+                    <th className="text-uppercase">Project type</th>
+                    <th className="text-uppercase">Project investor</th>
+                    <th className="text-uppercase">Address</th>
+                    <th className="text-uppercase">Land use term</th>
+                    <th className="text-uppercase">Total area</th>
+                    <th className="text-uppercase">Number of floor &amp; height</th>
+                    <th className="text-uppercase">Building density</th>
+                    <th className="text-uppercase text-center">Number of area</th>
+                    <th className="text-uppercase text-center"><i className="las la-cog" style={{}} /></th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.data ? props.data.map((item, index) => (
+                    trPerant(item, index)
+                )) : null}
+
+            </tbody>
+        </table>
+    )
+}
+
+export default ListProductTable;
