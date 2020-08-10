a6107b2ca698e67bc844ab96476a738e46b66994
HuyNguyen1701 <huynt@minerva.vn>
2020-08-10 08:09:15 +0700

    update menuitem link

--- src/components/SideBar/Menu/MenuItem.js
+++ src/components/SideBar/Menu/MenuItem.js
@@ -3,55 +3,89 @@ import { Trans } from "react-i18next";
 import { Link } from "react-router-dom";
 
 function strcmp(a, b) {
-  if (a.toString() < b.toString()) return -1;
-  if (a.toString() > b.toString()) return 1;
-  return 0;
+    if (a.toString() < b.toString()) return -1;
+    if (a.toString() > b.toString()) return 1;
+    return 0;
 }
 
 class MenuItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleClick = (e) => {
-    this.props.click(this.props.data.label);
-  };
-  routeTo = (e) => {
-    if (this.props.data.href) return this.props.data.href;
-  };
-  //sidebar-item active
-  render() {
-    const { data, children, click } = this.props;
-    var clsname = "sidebar-link";
-    if (data.children) {
-      clsname = clsname + " has-arrow";
+    constructor(props) {
+        super(props);
     }
-    if (data.active) {
-      clsname = clsname + " active";
-    }
-    return (
-      <li
-        className={data.active == true ? "sidebar-item active" : "sidebar-item"}
-        onClick={this.handleClick}
-      >
-        <Link to={this.props.data.href}>
-          <ul className={clsname}>
-            <i className={data.icon ? data.icon : "las la-home"}></i>
-            <span className="hide-menu">
-              <Trans>{data.label}</Trans>
-            </span>
-          </ul>
-        </Link>
 
-        <ul className="collapse first-level base-level-line">
-          {data.children &&
-            data.children.map((item, index) => (
-              <MenuItem key={index} data={item} click={click} />
-            ))}
-        </ul>
-      </li>
-    );
-  }
+    handleClick = (e) => {
+        this.props.click(this.props.data.label);
+    };
+    routeTo = (e) => {
+        if (this.props.data.href) return this.props.data.href;
+    };
+    //sidebar-item active
+    render() {
+        const { data, children, click } = this.props;
+        var clsname = "sidebar-link";
+        if (data.children) {
+            clsname = clsname + " has-arrow";
+        }
+        if (data.active) {
+            clsname = clsname + " active";
+        }
+        return (
+            <li
+                className={data.active == true ? "sidebar-item active" : "sidebar-item"}
+                onClick={this.handleClick}
+            >
+                {console.log(this.props.data.href)}
+                {this.props.data.href ?
+                    <Link to={this.props.data.href}>
+                        <ul className={clsname}>
+                            <i className={data.icon ? data.icon : "las la-home"}></i>
+                            <span className="hide-menu">
+                                <Trans>{data.label}</Trans>
+                            </span>
+                        </ul>
+
+
+                        <ul className="collapse first-level base-level-line">
+                            {data.children &&
+                                data.children.map((item, index) => (
+                                    <MenuItem key={index} data={item} click={click} />
+                                ))}
+                        </ul>
+                    </Link> :
+                    <div>
+                        <ul className={clsname}>
+                            <i className={data.icon ? data.icon : "las la-home"}></i>
+                            <span className="hide-menu">
+                                <Trans>{data.label}</Trans>
+                            </span>
+                        </ul>
+
+                        <ul className="collapse first-level base-level-line">
+                            {data.children &&
+                                data.children.map((item, index) => (
+                                    <MenuItem key={index} data={item} click={click} />
+                                ))}
+                        </ul>
+                    </div>
+                }
+                {/* <Link to={this.props.data.href}>
+                    <ul className={clsname}>
+                        <i className={data.icon ? data.icon : "las la-home"}></i>
+                        <span className="hide-menu">
+                            <Trans>{data.label}</Trans>
+                        </span>
+                    </ul>
+                </Link>
+
+                <ul className="collapse first-level base-level-line">
+                    {data.children &&
+                        data.children.map((item, index) => (
+                            <MenuItem key={index} data={item} click={click} />
+                        ))}
+                </ul> */}
+            </li>
+        );
+    }
 }
 //
 //( this.children ? 'sidebar-link active has-arrow' : 'sidebar-link active') :
