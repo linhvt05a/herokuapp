e24f4ed9b0d87bbd527846b536d3f7b3a918d5dc
HuyNguyen1701 <huynt@minerva.vn>
2020-08-12 13:56:36 +0700

    no message

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,9 +1,11 @@
 import Select from "./Select/Select";
 import Input from "./Input/Input";
-import TextEditor from "./Editor/TextEditor"
+import TextEditor from "./Editor/TextEditor";
+import * as Button from "./Button"
 
 export default {
     Select,
     Input,
-    TextEditor
+    TextEditor,
+    Button
 }
