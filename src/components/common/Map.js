853cd7b350c3ab112c14094a9dbe779b3c341682
TuanNguyen <tuannv@minerva.vn>
2020-08-19 15:23:34 +0700

    update maps home page

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import "leaflet/dist/leaflet.css";
 
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 function MapLea(props) {
     const position = [51.505, 0.09];
     return (
         <Map center={position} zoom={13} style={{ height: "670px", zIndex: 0 }}>
             <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={position}>
                 <Popup>
-                    A pretty CSS3 popup. <br/> Easily customizable.
+                    popup <br/> Easily customizable.
                 </Popup>
             </Marker>
         </Map>
