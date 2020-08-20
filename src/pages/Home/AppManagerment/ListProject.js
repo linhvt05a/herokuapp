8b3e62ea509f609668c16c0110f8d9876fa3cd1b
DESKTOP-N95HQV0\DELL <ducl471998@gmail.com>
2020-08-20 16:09:55 +0700

    test

---
+++ src/pages/Home/AppManagerment/ListProject.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+const ListProject = () => {
+  const list = [
+    {
+      name: "Quản lý dự án",
+      des:
+        "Xây dựng các công trình bất động sản chất lượng, đạt chuẩn cao cấp và đáp ứng yêu cầu của bạn",
+    },
+    {
+      name: "Quản lý dự án",
+      des:
+        "Xây dựng các công trình bất động sản chất lượng, đạt chuẩn cao cấp và đáp ứng yêu cầu của bạn",
+    },
+    {
+      name: "Quản lý dự án",
+      des:
+        "Xây dựng các công trình bất động sản chất lượng, đạt chuẩn cao cấp và đáp ứng yêu cầu của bạn",
+    },
+    {
+      name: "Quản lý dự án",
+      des:
+        "Xây dựng các công trình bất động sản chất lượng, đạt chuẩn cao cấp và đáp ứng yêu cầu của bạn",
+    },
+    {
+      name: "Quản lý dự án",
+      des:
+        "Xây dựng các công trình bất động sản chất lượng, đạt chuẩn cao cấp và đáp ứng yêu cầu của bạn",
+    },
+    {
+      name: "Quản lý dự án",
+      des:
+        "Xây dựng các công trình bất động sản chất lượng, đạt chuẩn cao cấp và đáp ứng yêu cầu của bạn",
+    },
+
+  ];
+  return (
+    <ul className="features">
+      {list.map((Item, index) => (
+        <ItemOverList key={index} item={Item} />
+      ))}
+    </ul>
+  );
+};
+
+const ItemOverList = (props) => {
+  const { item } = props;
+  return (
+    <li>
+      <p className="name">{item.name}</p>
+      <p className="des">{item.des}</p>
+    </li>
+  );
+};
+
+export default ListProject;
---
+++ src/pages/Home/AppManagerment/Name.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Name extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className="title text-uppercase">APP real ESTATE</h4>
+        <h5 className="sub-title">
+          Ứng dụng quản lý của chúng tôi với các tính năng nổi bật về việc quản
+          lý các thông tin bất động sản và kèm theo điều khiển các thiết bị
+          thông minh trong nhà như :
+        </h5>
+      </div>
+    );
+  }
+}
+
+export default Name;
