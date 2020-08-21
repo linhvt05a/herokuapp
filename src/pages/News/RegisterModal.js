e9acc506b1e1ec10a2b1136a6fb89d8f3d2edbe9
hoanglinh <>
2020-08-21 15:44:21 +0700

    update

--- src/pages/News/RegisterModal.js
+++ src/pages/News/RegisterModal.js
@@ -24,7 +24,7 @@ const RegisterModal = (props) =>{
   }
 
   return (
-          <Modal visible={false}  footer={null}>
+          <Modal visible={true}  footer={null}>
               <div  className="modal fade show" style={{ paddingRight: 15,display:'block'}}>
                 <div className="modal-dialog open" role="document" style={{ maxWidth: 395 }}>
                     <div className="modal-content">
