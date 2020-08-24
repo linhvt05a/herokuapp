b25c0c15b64d8dc305375f1de1a27b9f8e7be3a7
VanBui <vanbt@minerva.vn>
2020-08-24 19:10:43 +0700

    NOT YET

--- src/pages/ProductDetail/SideBar/Info/index.js
+++ src/pages/ProductDetail/SideBar/Info/index.js
@@ -2,99 +2,75 @@ import React, { Component } from 'react'
 import { Trans } from 'react-i18next';
 import Contact from '../Contact';
 
-class Info extends Component {
-    render() {
-        const { dataInfo } = this.props;
+const Info = (props) => {
+    const { dataInfo } = props;
+    console.log(dataInfo);
+    
         return (
+            dataInfo ?
             <div className="info">
                 {
-                    dataInfo.map((infoItem, index) => (
-                        <div key={index}>
-                            {
-                                infoItem.id == 1 ?
-                                    <div>
-                                        <div className="sales_heading">
-                                            <span className="text">{infoItem.title01}
-                                                <i className="icon_info fas fa-info-circle" />
-                                            </span>
-                                            <span className="number">{infoItem.listed_price}<Trans>đ</Trans></span>
-                                            <p className="compare_price">
-                                                <span className="num">{infoItem.compare_price}<Trans>đ</Trans></span>
-                                                <span className="discount_label"><Trans>Giảm</Trans>7<Trans>%</Trans></span>
-                                            </p>
-                                        </div>
-                                        <div className="info_1">
-                                            <div className="item">
-                                                <span className="title">
-                                                    {infoItem.title02}
-                                                    <i className="icon_info fas fa-info-circle" />
-                                                </span>
-                                                <span className="number water">
-                                                    {infoItem.clearance_price}
-                                                </span>
-                                                <span className="unit">
-                                                    <Trans>( đồng/m<sup>2</sup> )</Trans>
-                                                </span>
-                                            </div>
-                                            <div className="item">
-                                                <span className="title">
-                                                    {infoItem.title03}
-                                                    <i className="icon_info fas fa-info-circle" />
-                                                </span>
-                                                <span className="number acreage">
-                                                    {infoItem.dientich}
-                                                </span>
-                                                <span className="unit">
-                                                    <Trans>( m<sup>2</sup> )</Trans>
-                                                </span>
-                                            </div>
-                                            <div className="item">
-                                                <span className="title">
-                                                    {infoItem.title04}
-                                                </span>
-                                                <span className="number direction">
-                                                    {infoItem.huong}
-                                                </span>
-                                            </div>
-                                        </div>
-                                        <Contact/>
+                        <div>
+                            <div>
+                                <div className="sales_heading">
+                                    <span className="text">Giá niêm yết
+                                        <i className="icon_info fas fa-info-circle" />
+                                    </span>
+                                    <span className="number">{dataInfo.estimate_price}<Trans>đ</Trans></span>
+                                    
+                                        {/* {
+                                            dataInfo.price.map((item,index) => {
+                                                <p className="compare_price">
+                                                    <span className="num" key={index}>{item.estimate_price}<Trans>đ</Trans></span>
+                                                    <span className="discount_label"><Trans>Giảm</Trans>7<Trans></Trans>%</span>
+                                                </p>
+                                            })
+                                        } */}
+                                        
+                                   
+                                </div>
+                                <div className="info_1">
+                                    <div className="item">
+                                        <span className="title">
+                                            Giá thông thủy
+                                            <i className="icon_info fas fa-info-circle" />
+                                        </span>
+                                        <span className="number water">
+                                            {/* {infoItem.price_m2_use} */}
+                                        </span>
+                                        <span className="unit">
+                                            <Trans>( đồng/m<sup>2</sup> )</Trans>
+                                        </span>
                                     </div>
-                                    :
-                                    <div className="info_2">
-                                        <div className="heading">{infoItem.title}</div>
-                                        <table>
-                                            <tbody>
-                                                {
-                                                    infoItem.content.map((item, index) => {
-                                                        return (
-                                                            <tr key={index}>
-                                                                <td className="icon">
-                                                                    <i class="icon_label fas fa-couch"></i>
-                                                                </td>
-                                                                <td className="name">
-                                                                    <Trans>{item.room} ( m<sup>2</sup> )</Trans>
-                                                                </td>
-                                                                <td className="number">
-                                                                    {item.acreage}
-                                                                </td>
-                                                            </tr>
-                                                        )
-                                                    })
-                                                }
-                                            </tbody>
-                                        </table>
-                                        <div className="list_button d-flex">
-                                            <a href="/" className="btn btn_green text-uppercase"><Trans>MUA NGAY</Trans></a>
-                                            <a href="/" className="btn btn_purple text-uppercase"><Trans>NGÂN SÁCH</Trans></a>
-                                        </div>
-                                        <div className="see_bank_list"><a href="/"><Trans>XEM NGÂN HÀNG</Trans></a></div>
+                                    <div className="item">
+                                        <span className="title">
+                                            Diện tích
+                                            <i className="icon_info fas fa-info-circle" />
+                                        </span>
+                                        <span className="number acreage">
+                                            {/* {infoItem.area_m2_total} */}
+                                        </span>
+                                        <span className="unit">
+                                            <Trans>( m<sup>2</sup> )</Trans>
+                                        </span>
                                     </div>
-                            }
+                                    <div className="item">
+                                        <span className="title">
+                                            {/* {infoItem.title04} */}
+                                        </span>
+                                        <span className="number direction">
+                                            {/* {infoItem.huong} */}
+                                        </span>
+                                    </div>
+                                </div>
+                                <Contact/>
+                            </div>
+                                    
+                            
                         </div>
-                    ))
                 }
-            </div>
+            </div> : ""
         )
     }
-}
+
 export default Info;
--- src/pages/ProductDetail/SideBar/index.js
+++ src/pages/ProductDetail/SideBar/index.js
@@ -3,70 +3,17 @@ import Insentives from './Incentives';
 import Info from './Info';
 
 
-const dataInfo = [
-    {
-        id: 1,
-        title01: "Giá niêm yết",
-        listed_price: 2044474800,
-        compare_price: 2198360000,
-        content: [],
-        title02: "Giá thông thủy",
-        clearance_price: 50560000,
-        title03: "Diện tích",
-        dientich: 100,
-        title04: "Hướng",
-        huong: "Đông Bắc"
-    },
-    {
-        id: 2,
-        title: "Thông tin chi tiết",
-        content: [
-            {
-                icon: '<i class="icon_label fas fa-couch"></i>',
-                room: "Phòng khách",
-                acreage: 36
-            },
-            {
-                icon: 'https://via.placeholder.com/10x10',
-                room: "Phòng ngủ 1",
-                acreage: 16
-            },
-            {
-                icon: 'https://via.placeholder.com/10x10',
-                room: "Phòng ngủ 2",
-                acreage: 16
-            },
-            {
-                icon: 'https://via.placeholder.com/10x10',
-                room: "Phòng ngủ 3",
-                acreage: 15
-            },
-            {
-                icon: 'https://via.placeholder.com/10x10',
-                room: "Phòng vệ sinh 1",
-                acreage: 6
-            },
-            {
-                icon: 'https://via.placeholder.com/10x10',
-                room: "Phòng vệ sinh 2",
-                acreage: 7
-            },
-            {
-                icon: 'https://via.placeholder.com/10x10',
-                room: "Ban công",
-                acreage: 4
-            }
-        ]
-    }
-];
-class SiderBar extends Component {
-    render() {
+const SiderBar = (props) => {
+    const { dataInfo } = props;
+    console.log(dataInfo);
         return (
+            dataInfo ?
             <div className="apartment_detail">
                 <Info dataInfo={dataInfo} />
                 <Insentives />
             </div>
-        )
+            : ""
+        )        
     }
-}
+
 export default SiderBar;
