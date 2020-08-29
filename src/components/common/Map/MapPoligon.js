5ecebc31c214faee7f7df9d07d65245cf863495d
TuanNguyen <tuannv@minerva.vn>
2020-08-29 16:05:36 +0700

    update map

--- src/components/common/Map/MapPoligon.js
+++ src/components/common/Map/MapPoligon.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import ReactLeafletGoogleLayer from 'react-leaflet-google-layer';
@@ -9,13 +9,29 @@ import L from 'leaflet';
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    // iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('../../../../public/assets/images/marker.png'),
+    //shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 function MapPoligon(props) {
     let { data } = props;
+    // const createData = (project_name, lat, long, location) => {
+    //     return { project_name, lat, long, location }
+    // }
+
+    // const [state, setState] = useState({
+    //     data: []
+    // })
+
+    // if (data && data.length > 0) {
+    //     let newData = [];
+    //     data.map((item) => {
+    //         newData.push(createData(item.project_name, item.lat, item.long, item.location))
+    //     })
+    //     setState({ ...state, data: newData })
+    // }
+    // console.log(state);
     // data && data.map((item, index) => {
     //     console.log(item);
     // })
@@ -30,29 +46,29 @@ function MapPoligon(props) {
             
         // ))
         <Map 
-            center={position} zoom={18} 
+            center={position} zoom={5} 
             style={{ height: "670px", zIndex: 0 }}
             attributionControl={false}
-            >
-            <ReactLeafletGoogleLayer 
-                useGoogMapsLoade = {true}
-                googleMapsLoaderConf={{KEY: 'AIzaSyDZt7EWQR3cI-a_0to4VT2JVcF-c7ti6Ro'}} 
-                type={'hybrid'} 
-            />
-            {/* <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url=''
-            /> */}
-            <Marker position={position}>
-                <Popup>
-                    marker 1
-                </Popup>
-            </Marker>
-            <Marker position={center}>
-                <Popup>
-                    marker 2
-                </Popup>
-            </Marker>
+            position={'topright'}
+            // zoomControl={false}
+            >   
+                <ReactLeafletGoogleLayer 
+                    useGoogMapsLoade = {true}
+                    googleMapsLoaderConf={{KEY: 'AIzaSyDZt7EWQR3cI-a_0to4VT2JVcF-c7ti6Ro'}} 
+                    type={'hybrid'} 
+                />
+                {/* <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url=''
+                /> */}
+
+                {data && data.map((item, index) => (
+                    <Marker position={[item.lat, item.lon]} key={index} className="custom_marker_map">
+                        <Popup closeButton={false} className="custom_popup_map">
+                            {item.project_name}
+                        </Popup>
+                    </Marker>
+                ))} 
         </Map>
     );
 }
