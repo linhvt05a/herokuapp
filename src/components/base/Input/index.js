1ee11c966b9d42f4ddc6d5daf3592e25ac4906c7
hieunq <hieunq@minerva.vn>
2020-08-29 12:48:45 +0700

    add file index base

---
+++ src/components/base/Input/index.js
@@ -0,0 +1,8 @@
+import InputBase from './InputBase'
+import InputCheckbox from './InputCheckbox'
+import InputCheckboxViolet from './InputCheckboxViolet'
+import InputDatePicker from './InputDatePicker'
+
+export {
+    InputBase, InputCheckbox, InputCheckboxViolet, InputDatePicker
+}
