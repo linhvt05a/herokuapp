d1beba6b2a4562a5040b88e71496ab810c51dddc
vanbt <vanbt@minerva.vn>
2020-08-30 21:57:16 +0700

    EDIT PRODUCT_DETAIL NOT HAVE API

--- src/pages/ProductDetail/SalesAccordion/Accordion/AccordionItem.js
+++ src/pages/ProductDetail/SalesAccordion/Accordion/AccordionItem.js
@@ -1,87 +1,30 @@
-import React, { Component } from 'react';
 import Parser from 'html-react-parser';
-import TabsSlider from "../../TabsSlider";
-import MutiSlider from './../../../../components/common/MutiSlider';
+import React from 'react';
 import { Trans } from 'react-i18next';
+import SliderInTab from '../../TabsSlider/SliderInTab';
+import MutiSlider from './../../../../components/common/MutiSlider';
 
-const dataDemo = [
+const dataImgs = [
   {
-    id:2,
-    title: "Mặt bằng tầng và mặt bằng dự án",
-    paragraph : [
-      {
-        content: [
-          "./assets/images/living-room-area_01.jpg",
-          "https://via.placeholder.com/730x420",
-          "https://via.placeholder.com/730x420",
-          "https://via.placeholder.com/730x420",
-          "https://via.placeholder.com/730x420",
-          "https://via.placeholder.com/730x420",
-          "https://via.placeholder.com/730x420",
-        ]
-      }
-    ]
+      img: "./assets/images/bank02.jpg"
   },
   {
-    id: 3,
-    title: "Giới thiệu dự án và tiến độ",
-    paragraph : [
-      {
-        content: "Khu VALLEN gồm 3 tòa căn hộ, được thiết kế hiện đại với 6 loại hình căn hộ từ 29 - 95m2, đáp ứng được mọi nhu cầu của khách hàng. Phân khu Sapphire 3 được đánh số thứ tự (tính từ tuyến đường sắt đô thị số 6 vào công viên trung tâm) lần lượt là S3.01, S3.02, S3.03.",
-        video: `<iframe class="embed-responsive-item" width="680" height="500" src="https://www.youtube.com/embed/P_TT76GGhbU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
-      }
-    ]
+      img: "./assets/images/bank03.jpg"
   },
   {
-    id:4,
-    title: "Tiện ích nội khu",
-    paragraph : [
-      {
-        content: "Bể bơi người lớn"
-      },
-      {
-        content: "Bể bơi trẻ em"
-      },
-      {
-        content: "Đường dạo quanh bể bơi"
-      },
-      {
-        content: "Giàn hoa, ghế nghỉ"
-      },
-      {
-        content: "Vườn dưỡng sinh- chơi cờ"
-      },
-      {
-        content: "Vườn thiền"
-      },
-      {
-        content: "Thảm cỏ dưỡng sinh"
-      },
-      {
-        content: "Sân chơi trẻ em"
-      },
-      {
-        content: "Sân tập gym ngoài trời"
-      },
-      {
-        content: "Thảm cỏ đa năng"
-      },
-      {
-        content: "Chòi nghỉ cảnh quan"
-      }
-    ]
+      img: "./assets/images/bank04.jpg"
   },
   {
-    id:5,
-    title: "Ngân hàng hổ trợ liên kết",
-    paragraph : [
-      {
-        content: "Các ngân hàng đầy uy tính đã liên kết với chúng tôi trong việc cho bạn vay những khoản tiền phù hợp, để mua những sản phẩm của chúng tôi :"
-      }
-    ]
+      img: "./assets/images/bank05.jpg"
+  },
+  {
+      img: "./assets/images/bank06.jpg"
+  },
+  {
+      img: "./assets/images/bank07.jpg"
   }
-  
 ];
+
 
 
 class AccordionItem extends React.Component {
@@ -105,41 +48,19 @@ class AccordionItem extends React.Component {
       state: { opened }
     } = this;
 
+    const { data } = this.props;
+    const { itemImg }= this.props;
+    const MAX_SLIDES = 5; 
+    const infinite = itemImg? itemImg.length  > MAX_SLIDES : false;
     const settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
-
+        slidesToShow:MAX_SLIDES,
+        infinite:infinite,
+        swipeToSlide:true,
+        focusOnSelect:true
+    }
+    const {itemdata} = this.props;
+    const dataLayout = this.props.itemdata.layout;
+    const layout = itemdata.id===2?[{ title: "2d", content: dataLayout.layout_2d }, { title: "3d", content: dataLayout.layout_3d }, { title: "ar", content: dataLayout.layout_ar }]:""
     return (
       <div
         {...{
@@ -153,7 +74,8 @@ class AccordionItem extends React.Component {
           </div>
         </div>
         <div {...{ className: "collapse" }}>
-
+        {
+          data &&
           <div className="list_style_01">
             <div className="row">
               <div className="col-12 col-sm-6 col-sm-6 col-md-4">
@@ -192,61 +114,72 @@ class AccordionItem extends React.Component {
               </div>
             </div>
           </div>
-
-          
-          {/* 
-                id===2 ?
-                <div className="list_style_01">
-                    <div className="row">
-                      {
-                        paragraph.map((item,index) => (
-                          <div className="col-sm-12 col-sm-12 col-md-12" key={index}>
-                            <TabsSlider/>
-                          </div>
-                        ))
-                      }
-                    </div>
-                  </div> :
-                  id===3 ?
-                  <div className="list_style_01">
-                    <div className="row">
-                      {
-                        paragraph.map((item,index) => (
-                          <div className="col-sm-12 col-sm-12 col-md-12" key={index}>
-                            <p>{item.content}</p>
-                            <div class="embed-responsive embed-responsive-16by9">{Parser(item.video)}</div>
-                          </div>
-                        ))
-                      }
-                    </div>
-                  </div> :
-                  id===4 ?
-                  <div className="list_style_01">
-                    <div className="row">
-                      {
-                        paragraph.map((item,index) => (
-                          <div className="col-12 col-sm-6 col-sm-6 col-md-4" key={index}>
-                            <p className="list_style_02--item" key={index}>{item.content}</p>
-                          </div>
-                        ))
-                      }
-                    </div>
-                  </div> :
-                  <div className="list_style_01">
-                    <div className="row">
-                      {
-                        paragraph.map((item,index) => (
-                          <div className="col-sm-12 col-sm-12 col-md-12" key={index}>
-                            <p key={index}>{item.content}</p>
-                            <MutiSlider settings={settings} dataImgs={dataImgs}/>
-                          </div>
-                        ))
-                      }
-                    </div>
-                  </div> */}
-          {/* } */}
+          }
         </div>
-      </div>
+        
+
+        {/* DATA_SAMPLE */}
+        <div {...{ className: "card-header" }}>
+          <h3 {...{ className: "title" }}><Trans>Vị trí</Trans></h3>
+          <div {...{ onClick: () => { this.setState({ opened: !opened }); } }}>
+            <i class="icon_collapse fas fa-plus"></i>
+          </div>
+        </div>
+        <div {...{ className: "collapse" }}>
+        {
+          itemdata.id===2 ?
+            <div className="list_style_02">
+              <div className="row">
+                  <div className="col-sm-12 col-sm-12 col-md-12">
+                      {
+                          layout.map((itemlayout, index) => (
+                            <SliderInTab key={index} itemImg={itemlayout.content} />
+                        ))
+                      }
+                  </div>
+              </div>
+            </div> :
+            itemdata.id===3 ?
+            <div className="list_style_01">
+              <div className="row">
+                {
+                  itemdata.paragraph.map((item,index) => (
+                    <div className="col-sm-12 col-sm-12 col-md-12" key={index}>
+                      <p>{item.content}</p>
+                      <div class="embed-responsive embed-responsive-16by9">{Parser(item.video)}</div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div> :
+            itemdata.id===4 ?
+            <div className="list_style_01">
+              <div className="row">
+                {
+                  itemdata.paragraph.map((item,index) => (
+                    <div className="col-12 col-sm-6 col-sm-6 col-md-4" key={index}>
+                      <p className="list_style_02--item" key={index}>{item.content}</p>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+            :
+            <div className="list_style_01">
+              <div className="row">
+                {
+                  itemdata.paragraph.map((item,index) => (
+                    <div className="col-sm-12 col-sm-12 col-md-12" key={index}>
+                      <p key={index}>{item.content}</p>
+                      <MutiSlider settings={settings} dataImgs={dataImgs}/>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          }
+        </div>
+        </div>
     );
   }
 }
