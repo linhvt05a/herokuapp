7119675320f725d114cf61274a035f8af9fa0915
UyenDo <uyendtt@minerva.vn>
2020-09-04 16:12:22 +0700

    fix conflict

--- src/templates/ProductList/ProductRightBar.js
+++ src/templates/ProductList/ProductRightBar.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import {ProductForm, ProductPromotion} from './index'
-import PolicyAdvice from '../../components/common/PolicyAdvice'
 const ProductRightBar = (props) =>{
   const{data, showSignInModal, handleSignUp, handleChange, values, errors} = props
     return(
         <div className="col-12 col-sm-12 col-lg-4">
         
-          <PolicyAdvice heading="Priority policy" data={data}/>
+          {/* <PolicyAdvice heading="Priority policy" data={data}/> */}
         
       </div>
        
