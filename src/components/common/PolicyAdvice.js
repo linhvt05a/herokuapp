29c09f3b54099e19ec1edab762d61d2ae2d633aa
hoanglinh <>
2020-09-04 16:04:19 +0700

    UPDATE product favarite

--- src/components/common/PolicyAdvice.js
+++ src/components/common/PolicyAdvice.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Trans } from 'react-i18next';
 import CardNoData from '../common/CardNoData';
+import moment from 'moment'
 
-
+function convertDate (value){
+	const date = moment().format("DD/MM/YYYY")
+	return date
+}
 const PolicyAdvice = (props) => {
 	const {data, heading} = props;
+	console.log(data)
 	return (
 		<>
 			<div class="heading_purple">
@@ -15,27 +20,28 @@ const PolicyAdvice = (props) => {
 				<div className="tab-content mt-0">
 					<div className="tab-pane fade show active">
 					{
-						data ? 
-						<div className="incentives__user">
+						data && data.list_promotion_policy.length > 0 ? data.list_promotion_policy.map((item, index)=>
+						<div className="incentives__user" key={index}>
 							<div className="heading">
 								<span className="text">
-									{data.promotion_policy_name}
+									{item.promotion_policy_name}
 								</span>
 								<span className="date">
 									<i className="icon far fa-clock pr-1" />
-								<Trans>text_from</Trans>: {data.promotion_policy_start_date} 
+								<Trans>text_from</Trans>: {convertDate(item.promotion_policy_start_date)} 
 								<span className="pl-1 pr-1">-</span>  
-								<Trans>text_to</Trans>: {data.promotion_policy_end_date}
+								<Trans>text_to</Trans>: {convertDate(item.promotion_policy_end_date)}
 
 						</span>
 							</div>
 							<div className="list">
 							{/* promotion_policy_type_name noted */}
-								<p className="text">Giảm 2% giá trị</p>
-								<p className="text">Tặng 1 lượng vàng 9999</p>
-								<p className="text">Tặng gói bảo trì 1 năm căn hộ</p>
+								<p className="text"><Trans>{item.promotion_policy_type_name}</Trans></p>
+								<p className="text"><Trans>{item.promotion_policy_value_unit_name}</Trans></p>
+								<p className="text"><Trans>{item.promotion_policy_value}</Trans></p>
 							</div>
 						</div>
+						)
 						: <CardNoData />
 					}
 					</div>
--- src/templates/ProductList/ProductRightBar.js
+++ src/templates/ProductList/ProductRightBar.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import {ProductForm, ProductPromotion} from './index'
-
+import PolicyAdvice from '../../components/common/PolicyAdvice'
 const ProductRightBar = (props) =>{
   const{data, showSignInModal, handleSignUp, handleChange, values, errors} = props
     return(
         <div className="col-12 col-sm-12 col-lg-4">
-        <div className="row">
-          <ProductForm showSignInModal={showSignInModal} handleChange={handleChange} handleSignUp={handleSignUp}  values={values} errors={errors}/>
-          <ProductPromotion data={data}/>
-        </div>
+        
+          <PolicyAdvice heading="Priority policy" data={data}/>
+        
       </div>
        
     )
