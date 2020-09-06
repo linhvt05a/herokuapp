f17420dbf0660e04a51dd3bf34475ac03b7954b8
UyenDo <uyendtt@minerva.vn>
2020-09-06 12:23:16 +0700

    Revert "no message"
    This reverts commit f00f78bf02db276055057e032fe5f8a89745836e.

---
+++ src/templates/TransactionLevel/Container/index.js
@@ -0,0 +1,13 @@
+
+
+import BlockBankPolicy from './BlockBankPolicy';
+import BlockExchanges from './BlockExchanges';
+import BlockInternalUtility from './BlockInternalUtility';
+import BlockProjectPosition from './BlockProjectPosition';
+import BlockPromotionPolicy from './BlockPromotionPolicy';
+import BlockSearchProduct from './BlockSearchProduct';
+
+
+export {
+    BlockBankPolicy, BlockExchanges, BlockInternalUtility, BlockProjectPosition, BlockPromotionPolicy, BlockSearchProduct
+};
