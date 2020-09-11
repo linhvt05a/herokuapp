9a96ef561d01a8e54dd242190891d665f5d0fc3f
UyenDo <uyendtt@minerva.vn>
2020-09-11 15:42:49 +0700

    add variable for show_log

--- src/index.js
+++ src/index.js
@@ -7,12 +7,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import '../public/styles/cssLibs';
+import {MODE_ENV} from '../config/env';
 
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+if (!MODE_ENV.show_log)
+    console.log = () => {};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
