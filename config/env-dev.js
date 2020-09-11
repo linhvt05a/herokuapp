f406eb4885cfc1c7772270ffdb9ccb9701ddfb7e
UyenDo <uyendtt@minerva.vn>
2020-09-11 12:06:55 +0700

    update env config and inore env.js

---
+++ config/env-dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    NODE_ENV: 'none',
+    MODE_ENV: {
+        api: 'http://superapp.minerva.vn:9218',
+        cloudapi: 'https://cloudapi.minerva.vn',
+        port:3001
+    }
+};
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 
 // PROXY
 
-var env = require("../env");
+var env = require("../config/env");
 // console.log('CURRENT_ENV:', env.NODE_ENV);
 // console.log('MODE_ENV:', env.MODE_ENV);
 
