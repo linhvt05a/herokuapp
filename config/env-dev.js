71545626fd761e14ba7c4d39c21a061f88816eb3
HuyNguyen1701 <huynt@minerva.vn>
2020-09-14 17:45:17 +0700

    update env_dev

--- config/env-dev.js
+++ config/env-dev.js
@@ -3,18 +3,18 @@ module.exports = {
     MODE_ENV: {
         api: 'http://superapp.minerva.vn:9218',
         cloudapi: 'https://cloudapi.minerva.vn',
-        port:3001,
+        port: 3001,
 
         //dev
-        show_log:true,
-        hot:true,
-        liveReload:true,
-        host:'localhost',
+        // show_log:true,
+        // hot:true,
+        // liveReload:true,
+        // host:'localhost',
 
         //server
-        // show_log:false,
-        // hot:false,
-        // liveReload:false,
-        // host:'superapp.minerva.vn/'
+        show_log: false,
+        hot: false,
+        liveReload: false,
+        host: 'superapp.minerva.vn/'
     }
 };
