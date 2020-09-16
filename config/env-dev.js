b9cbc5ac57d69d1f4dbc6f805af9289d838ccca0
UyenDo <uyendtt@minerva.vn>
2020-09-16 10:05:14 +0700

    update env

--- config/env-dev.js
+++ config/env-dev.js
@@ -1,24 +1,40 @@
 module.exports = {
 	NODE_ENV: 'none',
 	MODE_ENV: {
-		api: 'http://superapp.minerva.vn:9218',
-		cloudapi: 'https://cloudapi.minerva.vn',
+		local:{
+			api: 'http://superapp.minerva.vn:9218',
+			cloudapi: 'https://cloudapi.minerva.vn',
 
 
-		//dev
-		show_log: true,
-		hot: true,
-		liveReload: true,
-		host: 'localhost',
-		port: 3001,
+			//dev
+			show_log: true,
+			hot: true,
+			liveReload: true,
+			host: 'localhost',
+			port: 3001,
 
-		// server
-		// show_log:false,
-		// hot:false,
-		// liveReload:false,
-		// host:'222.255.217.142',
-		// port:80,
-		// sockHost: 'superapp.minerva.vn',
-		// sockPort: 80,
+			// server
+			// show_log:false,
+			// hot:false,
+			// liveReload:false,
+			// host:'222.255.217.142',
+			// port:80,
+			// sockHost: 'superapp.minerva.vn',
+			// sockPort: 80,
+		},
+		development:{
+			api: 'http://superapp.minerva.vn:9218',
+			cloudapi: 'https://cloudapi.minerva.vn',
+			show_log: false,
+			hot: false,
+			liveReload: false,
+		},
+		production:{
+			api: 'http://superapp.minerva.vn:9218',
+			cloudapi: 'https://cloudapi.minerva.vn',
+			show_log: false,
+			hot: false,
+			liveReload: false,
+		},
 	}
 };
